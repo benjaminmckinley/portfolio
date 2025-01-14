a917e5dd0f5834d2dc12adc98a062e1ad847753a
Benjamin McKinley <benjamincmckinley@gmail.com>
2025-01-14 07:32:28 -0500

    Add Tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,13 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), tailwind({
+    nesting: true,
+  })],
   site: "https://example.com",
   experimental: {
     svg: true
