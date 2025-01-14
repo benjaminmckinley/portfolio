257293621d30693b4635aa15e9d0b6e9b82484fc
Benjamin McKinley <benjamincmckinley@gmail.com>
2025-01-13 21:22:07 -0500

    RSS feed configuration from documentation

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,22 @@
+import rss, { pagesGlobToRssItems } from "@astrojs/rss";
+import { getCollection } from "astro:content";
+
+export async function GET(context) {
+  const posts = await getCollection("blog");
+
+  return rss({
+    title: "Benjamin McKinley | Blog",
+    description: "TODO",
+    site: context.site,
+    // items: await pagesGlobToRssItems(import.meta.glob('./**/*.md')),
+    items: posts.map((post) => {
+      return {
+        title: post.data.title,
+        pubDate: post.data.pubDate,
+        description: post.data.description,
+        link: `/posts/${post.id}/`,
+      };
+    }),
+    customData: `<language>en-us</language>`,
+  });
+}
