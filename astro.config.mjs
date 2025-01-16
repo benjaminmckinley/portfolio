31b952c92755a7e15feaa2c344631ce7e4aacf89
Benjamin McKinley <benjamincmckinley@gmail.com>
2025-01-15 22:58:50 -0500

    Add sitemap + try to get deployed

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,14 @@ import { defineConfig } from "astro/config";
 
 import react from "@astrojs/react";
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-  site: "https://example.com",
+  integrations: [react(), sitemap()],
+  // site: "https://benjaminmckinley.com",
+  site: "https://benjaminmckinley.github.io",
+  base: 'portfolio',
   experimental: {
     svg: true,
   },
