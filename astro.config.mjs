053fc3b27b00f5893340619f845138e9d6d0b122
Benjamin McKinley <benjamincmckinley@gmail.com>
2025-01-20 16:09:34 -0500

    Markdown header links

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,26 @@ import {defineConfig} from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeSlug from "rehype-slug";
 
 // https://astro.build/config
 export default defineConfig({
     integrations: [react(), sitemap(), tailwind({
         nesting: true,
-    }),
+    },),
     ],
     // site: "https://benjaminmckinley.com",
     site: "https://benjaminmckinley.github.io",
     base: '.',
+    markdown: {
+        rehypePlugins: [rehypeSlug, [rehypeAutolinkHeadings, { behavior: 'prepend', content: {
+                type: 'element',
+                tagName: 'span',
+                children: [{ type: 'text', value: '# ' }]
+            }
+        }]],
+    },
     experimental: {
         svg: true,
     },
