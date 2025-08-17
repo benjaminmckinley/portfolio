ee62e3fda063e5e0c191e6fd41754a9d599e3926
Benjamin McKinley <benjamincmckinley@gmail.com>
2025-08-17 18:20:59 -0400

    Update CNAME + run prettier/eslint

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,38 @@
 // @ts-check
-import {defineConfig} from "astro/config";
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
-import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeSlug from "rehype-slug";
-import * as dotenv from "dotenv"
+import * as dotenv from "dotenv";
 
 dotenv.config();
 
 // https://astro.build/config
 export default defineConfig({
-    integrations: [react(), sitemap(), tailwind({
-        nesting: true,
-    },),
+  integrations: [
+    react(),
+    sitemap(),
+    tailwind({
+      nesting: true,
+    }),
+  ],
+  site: process.env.BASE_URL,
+  markdown: {
+    rehypePlugins: [
+      rehypeSlug,
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "prepend",
+          content: {
+            type: "element",
+            tagName: "span",
+            children: [{ type: "text", value: "#" }],
+          },
+        },
+      ],
     ],
-    site: process.env.BASE_URL,
-    base: '.',
-    markdown: {
-        rehypePlugins: [
-            rehypeSlug,
-            [rehypeAutolinkHeadings, {
-                 behavior: 'prepend', content: {
-                     type: 'element',
-                     tagName: 'span',
-                     children: [{type: 'text', value: '#'}],
-                 },
-             }],
-        ]
-    }
+  },
 });
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	corePlugins: {
-		preflight: false
-	},
-	plugins: [
-		require('@tailwindcss/typography')
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    preflight: false,
+  },
+  plugins: [require("@tailwindcss/typography")],
+};
